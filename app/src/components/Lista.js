8d8054e8463b9ee1ddf2c23c0b8494a4bd50bc0a
PcMain <hital12.ci@gmail.com>
2023-12-18 21:07:44 -0300

    nose

---
+++ app/src/components/Lista.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Lista() {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Climb Mt. Everest</li>
+      <li>Run a Marathon</li>
+      <li>Feed the dogs</li>
+    </ul>
+  );
+}
+
+export default Lista;
